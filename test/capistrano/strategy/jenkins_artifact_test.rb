ed22fb4297ced95a7ef9d88b27258011ce208505
Asato Wakisaka <asato.wakisaka@gmail.com>
2016-07-05 19:46:42 +0900

    make test work

--- test/capistrano/strategy/jenkins_artifact_test.rb
+++ test/capistrano/strategy/jenkins_artifact_test.rb
@@ -1,7 +1,4 @@
 require 'test_helper'
 
-class Capistrano::Strategy::JenkinsArtifactTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Capistrano::Strategy::JenkinsArtifact::VERSION
-  end
+class Capistrano::Deploy::Strategy::JenkinsArtifactTest < Minitest::Test
 end
